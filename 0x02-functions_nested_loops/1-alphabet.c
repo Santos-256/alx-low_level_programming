71fe6e64776e27c05b332a96aa106ca7b8caa4bc
Santos-256 <santosedward1998@gmail.com>
2023-08-16 02:08:25 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - This program prints 'a' to 'z'.
+ * Return: return 0;
+ */
+void print_alphabet(void)
+
+{
+
+
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+	_putchar(ch);
+	
+	}
+	_putchar('\n');
+
+}
