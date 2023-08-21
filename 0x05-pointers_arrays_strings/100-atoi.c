ff288a7e66b284249f35d62d6bfe651c4cfbe2f8
Santos-256 <santosedward1998@gmail.com>
2023-08-21 10:36:59 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,22 +16,23 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
-	while (i < len && f == 0)
+	while (i < len && (s[i] == '+' || s[i] == '-'))
 	{
 		if (s[i] == '-')
 			++d;
-		if (s[i] >= '0' && s[i] <= '9')
+		i++;
+	}
+
+		while (i < len && (s[i] >= '0' && s[i] <= '9'))
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (i + 1 < len && s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;
-		}
-		i++;
+			i++;
 	}
 	if (f == 0)
 		return (0);
