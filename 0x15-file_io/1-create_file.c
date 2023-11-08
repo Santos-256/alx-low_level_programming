1613c982a85156a11ffd665de21722b08e5615d6
Santos-256 <santosedward1998@gmail.com>
2023-11-08 06:50:30 -0800

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,21 +19,17 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
-
 	if (text_content != NULL)
 	{
 		for (longi = 0;  text_content[longi];)
 			longi++;
 	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	
 	wrt = write(fd, text_content, longi);
 
-	if (wrt == -1)
+	if (wrt == -1 || fd == -1)
 		return (-1);
 
 	close(fd);
