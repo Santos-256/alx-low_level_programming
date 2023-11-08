ac6325488ef0029ceb0ec8c8c524bec398217a33
Santos-256 <santosedward1998@gmail.com>
2023-11-08 04:44:11 -0800

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: name of the file to create
+ * @text_content: a pointer to a string to be written in the file
+ * as content
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int longi;
+	int wrt;
+
+	longi = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	wrt = write(fd, text_content, longi);
+
+	if (fd == -1)
+		return (-1);
+	if (wrt == -1)
+		return (-1);
+
+	if (!text_content)
+
+	for (longi = 0;  text_content[longi]; longi++)
+
+	close(fd);
+	return (1);
+}
