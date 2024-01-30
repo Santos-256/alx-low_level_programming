50df24e9ac61310124935554d18417887837917f
Santos-256 <santosedward1998@gmail.com>
2024-01-30 13:44:22 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int j;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
-			return (-1);
-
-
-		for (j = 0; j < size; j++)
-		{
-			if (cmp(array[j]))
-				return (j);
-		}
 		return (-1);
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]))
+			return (j);
+	}
+	return (-1);
 }
